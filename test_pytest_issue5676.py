02e5aee808d616d323d96cfe4e3c78150cf8e282
Matthew Raspberry <3092450+mraspberry@users.noreply.github.com>
2020-01-22 14:18:11 -0600

    Added test script for pytest issue 5676

---
+++ test_pytest_issue5676.py
@@ -0,0 +1,18 @@
+import time
+
+def proper():
+    with open(__file__) as fd:
+        return fd.read()
+
+def read_self():
+    return open(__file__).read()
+
+def test_read_self():
+    read_self()
+
+def test_proper():
+    proper()
+
+if __name__ == '__main__':
+    test_read_self()
+    test_proper()
